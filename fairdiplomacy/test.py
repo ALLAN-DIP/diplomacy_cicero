d79a69428e0941db854db6228918dbcfd574bf65
YanzeJeremy <yanzewan@usc.edu>
2023-02-22 17:31:12 -0600

    commit test

---
+++ fairdiplomacy/test.py
@@ -0,0 +1,44 @@
+import json
+from amrlib.models.parse_xfm.inference import Inference
+import sys
+sys.path.insert(0, '../AMR/DAIDE/DiplomacyAMR/code')
+from amrtodaide import AMR
+import regex
+def eng_to_daide():
+    #data = [json.loads(line) for line in open('RUSSIA_0.json', 'r')]
+    test_data = ''
+    count = 0
+    num_beams   = 4
+    batch_size  = 16
+    device = 'cpu'
+    model_dir  = '../AMR/amrlib/amrlib/data/model_parse_xfm/checkpoint-9920/'
+    print('loading models')
+    inference = Inference(model_dir, batch_size=batch_size, num_beams=num_beams, device=device)
+    print('generating')
+    gen_graphs = inference.parse_sents(["ITALY send that Hey Austria! I know AT is one of the least successful alliances in the game, so I'm super down to work together here, especially if we can get Russia and Turkey fighting."], disable_progress=False)
+    for graph in gen_graphs:
+        print(graph)
+        amr = AMR()
+        amr_node, s, error_list, snt_id, snt, amr_s = amr.string_to_amr(graph)
+        if amr_node:
+            amr.root = amr_node
+        amr_s2 = amr.amr_to_string()
+        if amr_s2 == '(a / amr-empty)':
+            daide_s, warnings = '', []
+        else:
+            daide_s, warnings = amr.amr_to_daide()
+            print(daide_s)
+        if regex.search(r'[A-Z]{3}', daide_s):
+            if regex.search(r'[a-z]', daide_s):
+                daide_status = 'Partial-DAIDE'
+            elif warnings:
+                daide_status = 'Para-DAIDE'
+            else:
+                daide_status = 'Full-DAIDE'
+        else:
+            daide_status = 'No-DAIDE'
+        print(daide_status)
+
+
+if __name__ == "__main__":
+    eng_to_daide()
