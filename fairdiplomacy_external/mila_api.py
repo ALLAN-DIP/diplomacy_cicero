5ec40a8e13f1a3258053c9575b9912d44d7e3c36
Wichayaporn Wongkamjan <wwongkam@login2.frontera.tacc.utexas.edu>
2023-02-21 22:48:05 -0600

    Remove duplicate test mila

--- fairdiplomacy_external/mila_api.py
+++ fairdiplomacy_external/mila_api.py
@@ -495,36 +495,6 @@ async def test_mila_function():
             await asyncio.sleep(1)
         curr_phase = game.get_current_phase()
 
-async def test_mila_function():
-    """ 
-    The function is to test ability that we can access Mila game on TACC 
-    Manually replace GAMEID and USERNAME to test accessing Mila game features
-    """
-
-    game_id = GAMEID
-    connection = await connect('shade.tacc.utexas.edu', 8432)
-    channel = await connection.authenticate(
-        USERNAME, "password"
-    )
-    game: NetworkGame = await channel.join_game(game_id=game_id, power_name="ENGLAND")
-
-    logging.info(f"Waiting for game to start")
-    # while game.is_game_forming:
-    #     await asyncio.sleep(2)
-    curr_phase = game.get_current_phase()
-    while not game.is_game_done:
-        print(f" game history {game.state_history}")
-        print(f" message history {game.message_history}")
-        print(f" order history {game.order_history}")
-        possible_orders = game.get_all_possible_orders()
-        ENG_orders = [random.choice(possible_orders[loc]) for loc in game.get_orderable_locations('ENGLAND')
-                    if possible_orders[loc]]
-        await game.set_orders(power_name='ENGLAND', orders=ENG_orders, wait=False)
-        while curr_phase == game.get_current_phase():
-            print(f" message in current phase {game.messages}")
-            await asyncio.sleep(1)
-        curr_phase = game.get_current_phase()
-
 if __name__ == "__main__":
     main()
     
