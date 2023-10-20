568775134fa72a15c6c4b0fad7b78203fdc6c63f
wwongkam <wwongkam@login2.ls6.tacc.utexas.edu>
2023-10-19 23:38:48 -0500

    add cicero intent experiment

---
+++ fairdiplomacy_external/cicero_intent_test.py
@@ -0,0 +1,4 @@
+# load agent config and player pyBQ
+# set up games to situation that Russia should betray Turkey
+# expect it to deceive with original intent?
+# expect it to deceive with new intent? 
